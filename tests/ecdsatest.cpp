b968bf8116cc3c9444776a762067f66074aa2d03
Maxim Mamontov <faust.madf@gmail.com>
2016-09-16 09:30:06 +0300

    Fix tests.

--- tests/ecdsatest.cpp
+++ tests/ecdsatest.cpp
@@ -7,7 +7,7 @@
 
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_GLOBAL_FIXTURE(InitOpenSSL);
+BOOST_GLOBAL_FIXTURE(InitOpenSSL)
 
 BOOST_AUTO_TEST_CASE(TestCtor256)
 {
@@ -22,7 +22,10 @@ BOOST_AUTO_TEST_CASE(TestCtor256)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     // ECDSA uses random nonce value, so the signature is always different and it is impossible to check its correctness by simple comparison. Due to the nature of ECDSA, non-random none makes possible recovery of the private key.
     // There is an RFS 6979 which provide ECDSA algorithm with deterministic nonce that solves problem of private key recovery.
-    BOOST_CHECK(!jwt.token("ecdsa-256-key-pair.pem").empty());
+    auto token = jwt.token("ecdsa-256-key-pair.pem");
+    auto part = token.substr(0, 56);
+    BOOST_CHECK(part == "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0" || part == "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJtYWRmIn0");
+    std::cout << token << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(TestCtor384)
@@ -38,7 +41,10 @@ BOOST_AUTO_TEST_CASE(TestCtor384)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     // ECDSA uses random nonce value, so the signature is always different and it is impossible to check its correctness by simple comparison. Due to the nature of ECDSA, non-random none makes possible recovery of the private key.
     // There is an RFS 6979 which provide ECDSA algorithm with deterministic nonce that solves problem of private key recovery.
-    BOOST_CHECK(!jwt.token("ecdsa-256-key-pair.pem").empty());
+    auto token = jwt.token("ecdsa-256-key-pair.pem");
+    auto part = token.substr(0, 56);
+    BOOST_CHECK(part == "eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0" || part == "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCJ9.eyJpc3MiOiJtYWRmIn0");
+    std::cout << token << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(TestCtor512)
@@ -54,13 +60,15 @@ BOOST_AUTO_TEST_CASE(TestCtor512)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     // ECDSA uses random nonce value, so the signature is always different and it is impossible to check its correctness by simple comparison. Due to the nature of ECDSA, non-random none makes possible recovery of the private key.
     // There is an RFS 6979 which provide ECDSA algorithm with deterministic nonce that solves problem of private key recovery.
-    BOOST_CHECK(!jwt.token("ecdsa-256-key-pair.pem").empty());
+    auto token = jwt.token("ecdsa-256-key-pair.pem");
+    auto part = token.substr(0, 56);
+    BOOST_CHECK(part == "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0" || part == "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzUxMiJ9.eyJpc3MiOiJtYWRmIn0");
+    std::cout << token << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(TestParser256Sig1)
 {
-    //JWTXX::JWT jwt("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.MEUCIERctp+BXGdF/NN+5ABMSdZJVXFwXnXrSbKqIKZYOB10AiEAuA+WLlW6ttNJ9p/bcdrr3wGsTAhbHL1uWvGqAmZsGSUA", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
-    JWTXX::JWT jwt("eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJtYW1vbnRvdiIsImlzcyI6IkltYWdpbmUiLCJleHAiOjE0NzM4NjU4MDJ9.31IlTQ_GHH6U-p0bW3NTzWYabbKR9_-2ngGU8fd-5ZKDV3Z6ekVDOX-vfnInJxWQ8y-OQn9ckIcvLhvSBo577A", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
+    JWTXX::JWT jwt("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.MEYCIQDLfrUl5UjzAJRoTSIHWYtrZgGeTVLe7Jbwr8VSdWPX1QIhAO44atlfe9nNKgoG5GjQSjBRFSRZnmfajNma_NVUJTQ4", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::ES256);
     BOOST_CHECK(!jwt.claims().empty());
@@ -76,7 +84,8 @@ BOOST_AUTO_TEST_CASE(TestParser256Sig1)
 
 BOOST_AUTO_TEST_CASE(TestParser256Sig2)
 {
-    JWTXX::JWT jwt("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.MEUCIAt6P9KZvF7xDJgWteEPFKOrzeBV6yrpCpcU21n39gTFAiEAxzKrbC0w7Vzt3eUCY5fIU1oOlXGpGU9JwE3Uqr2vxlMA", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
+    //JWTXX::JWT jwt("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.MEYCIQCAoUsDybFjeWYB3k12iNSLupk3I0iqxaQYPcoculvpIgIhAMgo1i0F7aPwCk2G3FquL7SpPpKtml76CASXgZxmC5Kv", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
+    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.MEUCIQCXgl-6ZJB5_xYHxrzvMVudzOcqo_sTMfSuIo3cwUofVgIgC2gWYZ3hig2v9HtzNRfnjE77cgBG3grXmv_qkmYmRc4A", JWTXX::Key(JWTXX::Algorithm::ES256, "public-ecdsa-256-key.pem"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::ES256);
     BOOST_CHECK(!jwt.claims().empty());
--- tests/hmactest.cpp
+++ tests/hmactest.cpp
@@ -7,7 +7,7 @@
 
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_GLOBAL_FIXTURE(InitOpenSSL);
+BOOST_GLOBAL_FIXTURE(InitOpenSSL)
 
 BOOST_AUTO_TEST_CASE(TestCtor256)
 {
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(TestCtor256)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W/6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3+ifw4");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W_6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3-ifw4");
 }
 
 BOOST_AUTO_TEST_CASE(TestCtor384)
@@ -38,7 +38,7 @@ BOOST_AUTO_TEST_CASE(TestCtor384)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn/6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt/e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn_6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt_e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
 }
 
 BOOST_AUTO_TEST_CASE(TestCtor512)
@@ -54,12 +54,12 @@ BOOST_AUTO_TEST_CASE(TestCtor512)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7/f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0./iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7_f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0._iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
 }
 
 BOOST_AUTO_TEST_CASE(TestParser256)
 {
-    JWTXX::JWT jwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W/6o", JWTXX::Key(JWTXX::Algorithm::HS256, "secret-key"));
+    JWTXX::JWT jwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W_6o", JWTXX::Key(JWTXX::Algorithm::HS256, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS256);
     BOOST_CHECK(!jwt.claims().empty());
@@ -70,12 +70,12 @@ BOOST_AUTO_TEST_CASE(TestParser256)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W/6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3+ifw4");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W_6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3-ifw4");
 }
 
 BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder256)
 {
-    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3+ifw4", JWTXX::Key(JWTXX::Algorithm::HS256, "secret-key"));
+    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3-ifw4", JWTXX::Key(JWTXX::Algorithm::HS256, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS256);
     BOOST_CHECK(!jwt.claims().empty());
@@ -86,12 +86,12 @@ BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder256)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W/6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3+ifw4");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.vIkGeF48t56EoWPeFUfd8fBddpS4GXtmA3K8Gs8W_6o" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYWRmIn0.JJvVBkQMTjGaQGiuE8YZ4PKYV5pTDE5NsPgOe3-ifw4");
 }
 
 BOOST_AUTO_TEST_CASE(TestParser384)
 {
-    JWTXX::JWT jwt("eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn/6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR", JWTXX::Key(JWTXX::Algorithm::HS384, "secret-key"));
+    JWTXX::JWT jwt("eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn_6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR", JWTXX::Key(JWTXX::Algorithm::HS384, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS384);
     BOOST_CHECK(!jwt.claims().empty());
@@ -102,12 +102,12 @@ BOOST_AUTO_TEST_CASE(TestParser384)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn/6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt/e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn_6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt_e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
 }
 
 BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder384)
 {
-    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt/e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY", JWTXX::Key(JWTXX::Algorithm::HS384, "secret-key"));
+    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt_e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY", JWTXX::Key(JWTXX::Algorithm::HS384, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS384);
     BOOST_CHECK(!jwt.claims().empty());
@@ -118,12 +118,12 @@ BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder384)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn/6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt/e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.oK6EMpKrAUGhQTvW6cd1LzO0RgMn_6BhJjy6jtSvkE3wies7LX3tRaHiPZDNnIMR" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.iQjt_e34Vti6ljweso9Vz4ajyujHlvrNhFiGt8y45gOsEQ80bNS5F3YtVkC570TY");
 }
 
 BOOST_AUTO_TEST_CASE(TestParser512)
 {
-    JWTXX::JWT jwt("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7/f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg", JWTXX::Key(JWTXX::Algorithm::HS512, "secret-key"));
+    JWTXX::JWT jwt("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7_f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg", JWTXX::Key(JWTXX::Algorithm::HS512, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS512);
     BOOST_CHECK(!jwt.claims().empty());
@@ -134,12 +134,12 @@ BOOST_AUTO_TEST_CASE(TestParser512)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7/f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0./iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7_f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0._iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
 }
 
 BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder512)
 {
-    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0./iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw", JWTXX::Key(JWTXX::Algorithm::HS512, "secret-key"));
+    JWTXX::JWT jwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0._iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw", JWTXX::Key(JWTXX::Algorithm::HS512, "secret-key"));
 
     BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::HS512);
     BOOST_CHECK(!jwt.claims().empty());
@@ -150,5 +150,5 @@ BOOST_AUTO_TEST_CASE(TestParserDifferentFieldOrder512)
     BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
     auto token = jwt.token("secret-key");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
-    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7/f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0./iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
+    BOOST_CHECK(token == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.I9H7VuaKsVIyYMRCIkl3YY0niNR5D9wnloF3jCfI7_f0Md4cr20tj2kV7tV5hrNMoTbSTP4SZM6AcfN4xCfvYg" || token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtYWRmIn0._iBZ2gZQsv5v2f8mZHKf45zUYYdCurzCyCf6UvRHGaOJTFUqJBMpL4UzZafnWQ9p27oAXgEgzlHyc0fWDydwKw");
 }
