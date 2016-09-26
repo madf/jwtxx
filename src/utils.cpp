60b6149cccdce3a7cee5ab9f9cfd96c94c9b57ed
Maxim Mamontov <faust.madf@gmail.com>
2016-09-26 09:52:36 +0300

    Read public keys from certs.

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,6 +3,7 @@
 #include "jwtxx/jwt.h" // Key::Error
 
 #include <openssl/pem.h>
+#include <openssl/x509.h>
 #include <openssl/err.h>
 
 #include <cstring> // strerror
@@ -13,9 +14,40 @@ namespace Utils = JWTXX::Utils;
 namespace
 {
 
+struct FileCloser
+{
+    void operator()(FILE* fp) { fclose(fp); }
+};
+typedef std::unique_ptr<FILE, FileCloser> FilePtr;
+
+struct X509Deleter
+{
+    void operator()(X509* cert) { X509_free(cert); }
+};
+typedef std::unique_ptr<X509, X509Deleter> X509Ptr;
+
 std::string sysError()
 {
     return strerror(errno);
+}
+
+Utils::EVPKeyPtr readPublicKey(const std::string& fileName)
+{
+    FilePtr fp(fopen(fileName.c_str(), "rb"));
+    if (!fp)
+        throw JWTXX::Key::Error("Can't open key file '" + fileName + "'. " + sysError());
+    return Utils::EVPKeyPtr(PEM_read_PUBKEY(fp.get(), nullptr, nullptr, nullptr));
+}
+
+Utils::EVPKeyPtr readCert(const std::string& fileName)
+{
+    FilePtr fp(fopen(fileName.c_str(), "rb"));
+    if (!fp)
+        throw JWTXX::Key::Error("Can't open key file '" + fileName + "'. " + sysError());
+    X509Ptr cert(PEM_read_X509(fp.get(), nullptr, nullptr, nullptr));
+    if (!cert)
+        return {};
+    return Utils::EVPKeyPtr(X509_get_pubkey(cert.get()));
 }
 
 }
@@ -33,12 +65,15 @@ Utils::EVPKeyPtr Utils::readPEMPrivateKey(const std::string& fileName)
 
 Utils::EVPKeyPtr Utils::readPEMPublicKey(const std::string& fileName)
 {
-    FilePtr fp(fopen(fileName.c_str(), "rb"));
-    if (!fp)
-        throw Key::Error("Can't open key file '" + fileName + "'. " + sysError());
-    EVPKeyPtr key(PEM_read_PUBKEY(fp.get(), nullptr, nullptr, nullptr));
+    auto key = readPublicKey(fileName);
+    std::string pkError;
     if (!key)
-        throw Key::Error("Can't read public key '" + fileName + "'. " + OPENSSLError());
+    {
+        pkError = OPENSSLError();
+        key = readCert(fileName);
+    }
+    if (!key)
+        throw Key::Error("File '" + fileName + "' is neither public key (" + pkError + ") nor certificate (" + OPENSSLError() + ").");
     return key;
 }
 
