68bdf2352f798f0de1269d6af1ac7e53e6f887fb
Maxim Mamontov <madf@madf.info>
2016-10-08 14:52:09 +0300

    Add more tests for RSA.

--- tests/rsatest.cpp
+++ tests/rsatest.cpp
@@ -16,6 +16,8 @@ constexpr const char token384Order1[] = "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.ey
 constexpr const char token384Order2[] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzM4NCJ9.eyJpc3MiOiJtYWRmIn0.tU5SWVRjc2BgVdZN4heYolUFhDIc9dHOxw7zYFstZKwEbj_K_9IRsl2kJ9Nw4NXGBntkBMIp6U1cqW9xkgiH1u4IAalL0q0NBIOW5-JEqUO65iDaDLkObJY65tbDT2q-CpsCBlYpULcWaZWj-3Xjn3Z0oTItjn40c4BJ8Ra0MaKDRKZNMScMnV7mHKylsPe-0aiRWK5QMaW_4KUjKmcJHpqY0aCA1yov1RLauPhXFC3w4KU_BbLJN25fMkSNwapmPKC2NQxuGjQRglAlq-XbxMe1FKieLPGCAPJLkzm9BHkZ0mmm405jVCfxEAw5DOVRrP5xo_2mpOxjIbBXJFPWMg";
 constexpr const char token512Order1[] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJtYWRmIn0.G8vv-IyeoIg5SWcN1Qb3-9kLPr8tnUSbBPO32Srvce04Lfe0sEWmlbBD5FR7dTYtpd2iMLwu7ytFsb0n_pOD1_ti_XAytQvJH6w6pL0saABs8y_Gj1AWONfIqGGw-2ChgItUsGpHt3yr7i4gV6bKYtU2KFcwFQ3c7bXpLjV9paptLQNwOHTjadmNcuexV443pz1NlaOP9DUjTzO2WNaiAokNtJ4wh6Emdjors844p4bCnah0o4STUYbcvRILYMVIpxrew7lcrLiA-vF8kD-Pxeb_Gna8sTIQ3gx7K1fL10Mg0wPEake4Qko29gy8AXx1qki5hEYx9Pe8CxezBfyFmA";
 constexpr const char token512Order2[] = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJtYWRmIn0.fyDpJVopst2qpHzs63NRWZ9zY04KrxfQmiF0zt3i-ACB38hnBo90r_PlrhMiMU8HU52_JxcBUAi5lSQHakX16EQyR0pEwZZfoCbeI0lOzpt0yrXAYyGvJC8WlE1Mm9Ehd8iUgcvvc9nL0_gRT-4RSU7kVOHOlwIRiN6fE28si22Jjtb5hf0KjebLj7BTeNnVB17SK9qMMn7VuYtRKj767FDpnZuOwNkDvb2qeL3IzbJnTYgpOVMoinHWAvR_innn9BjzCOEkFqgj6KKzQiEbVI6tbHfD-qOHHVDF1a0eERtCUuu92LesUyWJj3etgnmmNFI5EVjDtQkldQuuFgARUw";
+constexpr const char tokenCorruptedSign[] = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwiaXNzIjoibWFkZiIsImlhdCI6MTQ3NTI0MjkyMywibmJmIjoxNDc1MjQyOTIzLCJleHAiOjE0NzUyNDY1MjN9.r8mj1m0XYra8hRg2e-E85N75gEGAWbcqIMjQunFleW9XmbqAdC9YYKbfLQRe0MTXTYjP4lsfZdo7fWW93dTOpA5IpcPKAZe53GPozs1bz7GvscElUrXusVR345v4TiCk_CTAvr6DqX5o44i6OP9og5lgguXja-u_UvormjeUTA1ASM3vsQ1mTmUx8iUExnmrXrAtgtSZe_a1ebOYmZd0-cpA82zT8BlrJes_Po35Qwe3wmtvY5EZBwpT6bgEss9QOuUtqXIVZYp0HhkfD5dkpzbS9j60bxlYfb7iVDPLRx1_1iLa7ehja-kkGOvcmJcFL-";
+constexpr const char tokenWithExp[] = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwiaXNzIjoibWFkZiIsImlhdCI6MTQ3NTI0MjkyMywibmJmIjoxNDc1MjQyOTIzLCJleHAiOjE0NzUyNDY1MjN9.r8mj1m0XYra8hRg2e-E85N75gEGAWbcqIMjQunFleW9XmbqAdC9YYKbfLQRe0MTXTYjP4lsfZdo7fWW93dTOpA5IpcPKAZe53GPozs1bz7GvscElUrXusVR345v4TiCk_CTAvr6DqX5o44i6OP9og5lgguXja-u_UvormjeUTA1ASM3vsQ1mTmUx8iUExnmrXrAtgtSZe_a1ebOYmZd0-cpA82zT8BlrJes_Po35Qwe3wmtvY5EZBwpT6bgEss9QOuUtqXIVZYp0HhkfD5dkpzbS9j60bxlYfb7iVDPLRx1_1iLa7ehja-kkGOvcmJcFL-KxbZ9EjHAnVsB7jtDt3A";
 
 }
 
@@ -259,4 +261,60 @@ BOOST_AUTO_TEST_CASE(TestParserWithCertDifferentFieldOrder512)
     auto token = jwt.token("rsa-2048-key-pair.pem");
     // Jansson uses hashtables form JSON objects and hash function implementation reads over the boundary of the string, yet word-aligned, so actual order of header fields and claims is undefined.
     BOOST_CHECK(token == token512Order1 || token == token512Order2);
+}
+
+BOOST_AUTO_TEST_CASE(TestVerifier)
+{
+    BOOST_CHECK(JWTXX::JWT::verify(token512Order2, JWTXX::Key(JWTXX::Algorithm::RS512, "public-rsa-2048-key.pem")));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246522)}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246524)}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::iat(1475242922)}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::iat(1475242924)}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::nbf(1475242922)}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::nbf(1475242924)}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246522), JWTXX::Validate::iat(1475246522), JWTXX::Validate::nbf(1475246522)}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246524), JWTXX::Validate::iat(1475246524), JWTXX::Validate::nbf(1475246524)}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475242922), JWTXX::Validate::iat(1475242922), JWTXX::Validate::nbf(1475242922)}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::iss("madf")}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::iss("somebody")}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::sub("user")}));
+    BOOST_CHECK(!JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::sub("someone")}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::aud("")}));
+    BOOST_CHECK(JWTXX::JWT::verify(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::aud("something")})); // Audience is missing in the token
+}
+
+BOOST_AUTO_TEST_CASE(TestParserNoVerify)
+{
+    auto jwt = JWTXX::JWT::parse(tokenWithExp);
+    BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::RS256);
+    BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
+    BOOST_CHECK_EQUAL(jwt.claim("sub"), "user");
+    BOOST_CHECK_EQUAL(jwt.claim("exp"), "1475246523");
+    BOOST_CHECK_EQUAL(jwt.claim("iat"), "1475242923");
+    BOOST_CHECK_EQUAL(jwt.claim("nbf"), "1475242923");
+}
+
+BOOST_AUTO_TEST_CASE(TestParserNoVerifyCorruptedSignature)
+{
+    auto jwt = JWTXX::JWT::parse(tokenCorruptedSign);
+    BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::RS256);
+    BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
+    BOOST_CHECK_EQUAL(jwt.claim("sub"), "user");
+    BOOST_CHECK_EQUAL(jwt.claim("exp"), "1475246523");
+    BOOST_CHECK_EQUAL(jwt.claim("iat"), "1475242923");
+    BOOST_CHECK_EQUAL(jwt.claim("nbf"), "1475242923");
+}
+
+BOOST_AUTO_TEST_CASE(TestParserExtraVerification)
+{
+    JWTXX::JWT jwt(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246522), JWTXX::Validate::iat(1475246522), JWTXX::Validate::nbf(1475246522)});
+    BOOST_CHECK_EQUAL(jwt.alg(), JWTXX::Algorithm::RS256);
+    BOOST_CHECK_EQUAL(jwt.claim("iss"), "madf");
+    BOOST_CHECK_EQUAL(jwt.claim("sub"), "user");
+    BOOST_CHECK_EQUAL(jwt.claim("exp"), "1475246523");
+    BOOST_CHECK_EQUAL(jwt.claim("iat"), "1475242923");
+    BOOST_CHECK_EQUAL(jwt.claim("nbf"), "1475242923");
+    BOOST_CHECK_THROW(JWTXX::JWT(tokenCorruptedSign, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem")), JWTXX::JWT::Error);
+    BOOST_CHECK_THROW(JWTXX::JWT(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475242922), JWTXX::Validate::iat(1475242922), JWTXX::Validate::nbf(1475242922)}), JWTXX::JWT::Error);
+    BOOST_CHECK_THROW(JWTXX::JWT(tokenWithExp, JWTXX::Key(JWTXX::Algorithm::RS256, "public-rsa-2048-key.pem"), {JWTXX::Validate::exp(1475246524), JWTXX::Validate::iat(1475246524), JWTXX::Validate::nbf(1475246524)}), JWTXX::JWT::Error);
 }
