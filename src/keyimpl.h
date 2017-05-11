0b08a3698e0cc1a26d7a85238c5035ceeb564e3a
Maxim Mamontov <faust.madf@gmail.com>
2017-05-11 22:18:46 +0300

    Enable move and disable copy for key implementations.

--- src/keyimpl.h
+++ src/keyimpl.h
@@ -9,7 +9,15 @@ namespace JWTXX
 
 struct Key::Impl
 {
-    virtual ~Impl() {}
+    // Need this for polymorphic behavior.
+    virtual ~Impl() = default;
+    // Need this due to the Rule of Five.
+    Impl() = default;
+    Impl(const Impl&) = delete;
+    Impl& operator=(const Impl&) = delete;
+    Impl(Impl&&) = default;
+    Impl& operator=(Impl&&) = default;
+
     virtual std::string sign(const void* data, size_t size) const = 0;
     virtual bool verify(const void* data, size_t size, const std::string& signature) const = 0;
 };
